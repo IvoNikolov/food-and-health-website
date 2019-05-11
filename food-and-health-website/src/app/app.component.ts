22a291886e8ba3699f35c8a980c6d8730968d51d
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-05-11 23:36:21 +0300

    Added changes to initial commit

--- food-and-health-website/src/app/app.component.ts
+++ food-and-health-website/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'food-and-health-website';
+  title = 'Food & Health';
+  data = 'info';
+  constructor() {
+
+  }
+
 }
