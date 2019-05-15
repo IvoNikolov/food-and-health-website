723205abdec423759e8f8cc87403d8a6e8c66d2a
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-05-15 22:28:39 +0300

    Adding components

--- food-and-health-website/src/app/app.module.ts
+++ food-and-health-website/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
@@ -18,6 +20,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
 
   ],
   imports: [
