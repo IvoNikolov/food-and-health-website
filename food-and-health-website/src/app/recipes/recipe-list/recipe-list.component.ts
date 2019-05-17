1405cb8458e65cba3e1ee3a0bad8565d3bf9bfbe
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-05-17 22:39:48 +0300

    Fixes to bootstrap

--- food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Soup', 'Homemade soup',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg')
+  ];
 
   constructor() { }
 
