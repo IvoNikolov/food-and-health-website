92d5e9f2917e489c8a2bb7ee7ec7636915bc5816
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-05-24 20:08:57 +0300

    Adding recipe list and shopping list

--- food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected =  new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Soup', 'Homemade soup',
     'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg'),
-    new Recipe('Soup', 'Homemade soup',
-    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg')
+    new Recipe('Pizza', 'Homemade pizza',
+    'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
