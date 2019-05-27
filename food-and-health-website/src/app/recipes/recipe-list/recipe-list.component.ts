feaa5a32c569339848630b4dc8957c08c715f267
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-05-27 23:30:01 +0300

    Using Recipe Service within the app

--- food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,16 +10,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected =  new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Soup', 'Homemade soup',
-    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg'),
-    new Recipe('Pizza', 'Homemade pizza',
-    'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipe();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- food-and-health-website/src/app/recipes/recipe.service.ts
+++ food-and-health-website/src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('Soup', 'Homemade soup',
+        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg'),
+        new Recipe('Pizza', 'Homemade pizza',
+        'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg')
+      ];
+
+      getRecipe() {
+          return this.recipes.slice();
+      }
 }
