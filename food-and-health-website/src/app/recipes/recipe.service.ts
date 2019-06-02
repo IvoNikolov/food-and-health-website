5191526f50d240f98649eba98e3431a325141cf2
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-02 17:06:53 +0300

    Adding ingredients

--- food-and-health-website/src/app/recipes/recipe.service.ts
+++ food-and-health-website/src/app/recipes/recipe.service.ts
@@ -1,15 +1,27 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Soup', 'Homemade soup',
-        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg'),
-        new Recipe('Pizza', 'Homemade pizza',
-        'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg')
+        new Recipe('Soup',
+        'Homemade soup',
+        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg',
+        [
+            new Ingredient('Tomatoes', 2),
+            new Ingredient('Potatoes', 1)
+        ]),
+        new Recipe('Pizza',
+        'Homemade pizza',
+        'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+        [
+            new Ingredient('Tomatoes', 2),
+            new Ingredient('Salami', 1),
+            new Ingredient('Doe', 1)
+        ])
       ];
 
       getRecipe() {
