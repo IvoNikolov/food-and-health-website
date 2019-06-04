640df1303f658a647718561621f71da19681818a
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-04 22:38:41 +0300

    Adding routing and fixing reloading of the web app

---
+++ food-and-health-website/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const AppRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(AppRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
