4acd8f9c4ae15a009cacb2bf5267543da3b0aaee
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-05 21:19:58 +0300

    Adding chield pages

--- food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-and-health-website/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,7 +14,7 @@ export class RecipeListComponent implements OnInit {
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipes = this.recipeService.getRecipe();
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
--- food-and-health-website/src/app/recipes/recipe.service.ts
+++ food-and-health-website/src/app/recipes/recipe.service.ts
@@ -30,8 +30,12 @@ export class RecipeService {
 
     }
 
-    getRecipe() {
+    getRecipes() {
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
     }
 
     addingredientsToShoppingList(ingredients: Ingredient[]) {
