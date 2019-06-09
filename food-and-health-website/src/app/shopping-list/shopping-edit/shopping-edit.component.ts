fa80e0c836bce64eb9f3b1621b8f387486f53448
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-09 19:01:48 +0300

    Adding add and updating functionality to shopping list items

--- food-and-health-website/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ food-and-health-website/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -31,10 +31,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       });
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(form.value.name, form.value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy() {
--- food-and-health-website/src/app/shopping-list/shopping-list.service.ts
+++ food-and-health-website/src/app/shopping-list/shopping-list.service.ts
@@ -2,29 +2,34 @@ import { Ingredient } from '../shared/ingredient.model';
 import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-    ingredientsChanged = new Subject<Ingredient[]>();
-    startedEditing = new Subject<number>();
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
-    private ingredients: Ingredient[] = [
-        new Ingredient('Potato', 5),
-        new Ingredient('Onion', 2 )
-      ];
+  private ingredients: Ingredient[] = [
+      new Ingredient('Potato', 5),
+      new Ingredient('Onion', 2 )
+    ];
 
-      getIngredients() {
-        return this.ingredients.slice();
-      }
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 
-      getIngredient(index: number) {
-        return this.ingredients[index];
-      }
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
 
-      addIngredient(ingredient: Ingredient) {
-        this.ingredients.push(ingredient);
-        this.ingredientsChanged.next(this.ingredients.slice());
-      }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 
-      addIngredients(ingredients: Ingredient[]) {
-        this.ingredients.push(...ingredients);
-        this.ingredientsChanged.next(this.ingredients.slice());
-      }
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
