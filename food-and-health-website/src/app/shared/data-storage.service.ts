061aa4eeeb55dcafabc616b88bd9dc43b7985265
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-11 20:36:13 +0300

    Adding firebase and uploading data

---
+++ food-and-health-website/src/app/shared/data-storage.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({providedIn: 'root'})
+
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipeService: RecipeService) {
+
+    }
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-recipe-book-ad064.firebaseio.com/recipes.json', recipes).subscribe(response => {
+            console.log(response);
+        });
+    }
+}
