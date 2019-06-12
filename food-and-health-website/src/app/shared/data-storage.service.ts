29d5cfc3f596885366ed6c34f55df85fe76f65ad
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-12 08:35:54 +0300

    Adding resolver

--- food-and-health-website/src/app/shared/data-storage.service.ts
+++ food-and-health-website/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 
@@ -19,14 +19,15 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://ng-recipe-book-ad064.firebaseio.com/recipes.json')
+        return this.http.get<Recipe[]>('https://ng-recipe-book-ad064.firebaseio.com/recipes.json')
         .pipe(map(recipes => {
             return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
             });
-        }))
-        .subscribe(recipes => {
-            this.recipeService.setRecipes(recipes);
-        });
+        }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
+        );
     }
 }
