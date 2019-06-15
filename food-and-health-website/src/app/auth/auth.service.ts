82c3dcb12c2cc799ee287482a0541041895031b2
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-15 15:37:09 +0300

    Adding login functionality

--- food-and-health-website/src/app/auth/auth.service.ts
+++ food-and-health-website/src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expireIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -27,16 +28,37 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occured!';
-            if (!errorRes.error || !errorRes.error.error) {
-                return throwError(errorMessage);
-            } else {
-                switch (errorRes.error.error.message) {
-                    case 'EMAIL_EXISTS' : errorMessage = 'This email already exists!';
-                }
-                return throwError(errorMessage);
+        ).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+       return this.http.post<AuthResponseData>(
+           'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCjrXVAPdtyXt86JKnpNTj8xdCP-zrY708',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
             }
-        }));
+       ).pipe(catchError(this.handleError));
+    }
+
+    private handleError( errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occured!';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        } else {
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS' :
+                    errorMessage = 'This email already exists!';
+                    break;
+                case 'EMAIL_NOT_FOUND' :
+                    errorMessage = 'This email does not exists!';
+                    break;
+                case 'INVALID_PASSWORD' :
+                    errorMessage = 'This password is not correct!';
+                    break;
+            }
+            return throwError(errorMessage);
+        }
     }
 }
