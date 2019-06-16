7c2ab13f3e70163a2b49ead1b0b07f413dac8b8c
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-16 12:01:07 +0300

    Adding Authentication

--- food-and-health-website/src/app/auth/auth.component.ts
+++ food-and-health-website/src/app/auth/auth.component.ts
@@ -1,7 +1,11 @@
 import { Component, ɵConsole } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService, AuthResponseData } from './auth.service';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
+
+
 
 @Component({
     selector: 'app-auth',
@@ -13,7 +17,7 @@ export class AuthComponent {
     isLoading = false;
     error: string = null;
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
@@ -37,6 +41,7 @@ export class AuthComponent {
         authObs.subscribe(resData => {
             console.log(resData);
             this.isLoading = false;
+            this.router.navigate(['/recipes']);
         }, errorMessage => {
             console.log(errorMessage);
             this.error = errorMessage;
--- food-and-health-website/src/app/header/header.component.ts
+++ food-and-health-website/src/app/header/header.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+    isAuthenticated = false;
+    private userSub: Subscription;
 
-    constructor(private dataStorageService: DataStorageService ) {}
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+    ngOnInit() {
+        this.userSub = this.authService.user.subscribe(user => {
+            this.isAuthenticated = !!user;
+        });
+    }
 
     onSaveData() {
         this.dataStorageService.storeRecipes();
@@ -18,4 +30,7 @@ export class HeaderComponent {
         this.dataStorageService.fetchRecipes().subscribe();
     }
 
+    ngOnDestroy() {
+        this.userSub.unsubscribe();
+    }
 }
