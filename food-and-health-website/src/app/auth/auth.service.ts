13d760a66a7c5edf727efaf6c5a3678c492b00d0
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-06-16 23:33:27 +0300

    Adding save login in local storage feature and fixing issue with missing expiration date for the token

--- food-and-health-website/src/app/auth/auth.service.ts
+++ food-and-health-website/src/app/auth/auth.service.ts
@@ -10,7 +10,7 @@ export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
-    expireIn: string;
+    expiresIn: string;
     localId: string;
     registered?: boolean;
 }
@@ -33,7 +33,7 @@ export class AuthService {
                 returnSecureToken: true
             }
         ).pipe(catchError(this.handleError), tap(resData => {
-            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expireIn);
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
         }));
     }
 
@@ -46,8 +46,26 @@ export class AuthService {
                 returnSecureToken: true
             }
        ).pipe(catchError(this.handleError), tap(resData => {
-            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expireIn);
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
        }));
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
     }
 
     logout() {
@@ -56,9 +74,10 @@ export class AuthService {
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
-        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000));
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError( errorRes: HttpErrorResponse) {
--- food-and-health-website/src/app/auth/user.model.ts
+++ food-and-health-website/src/app/auth/user.model.ts
@@ -4,7 +4,7 @@ export class User {
     }
 
     get token() {
-        if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate){
+        if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
             return null;
         }
 
